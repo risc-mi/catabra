b7ab5e6183f79228e32514658c5b00749029855a
Alexander Maletzky <alexander.maletzky@risc-software.at>
2022-11-08 13:22:25 +0100

    o Moved subclasses of Invocation to main.py

--- catabra/analysis/main.py
+++ catabra/analysis/main.py
@@ -3,11 +3,10 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 
-from .config import AnalysisInvocation
 from ..ood.base import OODDetector
 from ..util import table as tu, io, logging, plotting, statistics
 from ..util import common as cu
-from ..core.base import CaTabRaBase
+from ..core.base import Invocation, CaTabRaBase
 from ..core import config as cfg
 from ..util.encoding import Encoder
 from ..automl.base import AutoMLBackend
@@ -72,7 +71,7 @@ def analyze(*table: Union[str, Path, pd.DataFrame], classify: Optional[Iterable[
 class Analyzer(CaTabRaBase):
 
     @property
-    def invocation_class(self) -> Type[AnalysisInvocation]:
+    def invocation_class(self) -> Type['AnalysisInvocation']:
         return AnalysisInvocation
 
     def _call(self):
@@ -408,3 +407,131 @@ class Analyzer(CaTabRaBase):
         else:
             text = None
         return dict(training_history=backend.training_history(x, [hist[m] for m in ms], legend=ms, text=text))
+
+
+class AnalysisInvocation(Invocation):
+
+    @property
+    def classify(self) -> Optional[Iterable[Union[str, Path, pd.DataFrame]]]:
+        return self._classify
+
+    @property
+    def regress(self) -> Optional[Iterable[Union[str, Path, pd.DataFrame]]]:
+        return self._regress
+
+    @property
+    def target(self) -> Optional[Iterable[str]]:
+        return self._target
+
+    @property
+    def group(self) -> Optional[str]:
+        return self._group
+
+    @property
+    def time(self) -> Optional[list]:
+        return self._time
+
+    @property
+    def ignore(self) -> Optional[Iterable[str]]:
+        return self._ignore
+
+    @property
+    def config_src(self) -> Union[str, Path, dict, None]:
+        return self._config_src
+
+    @config_src.setter
+    def config_src(self, value: Union[str, Path, dict, None]):
+        self._config_src = value
+
+    @property
+    def default_config(self) -> Optional[str]:
+        return self._default_config
+
+    def __init__(
+        self,
+        *table: Union[str, Path, pd.DataFrame],
+        sample_weight: Optional[str] = None,
+        out: Union[str, Path, None] = None,
+        jobs: Optional[int] = None,
+        classify: Optional[Iterable[Union[str, Path, pd.DataFrame]]] = None,
+        regress: Optional[Iterable[Union[str, Path, pd.DataFrame]]] = None,
+        group: Optional[str] = None,
+        split: Optional[str] = None,
+        time: Optional[str] = None,
+        ignore: Optional[Iterable[str]] = None,
+        config: Union[str, Path, dict, None] = None,
+        default_config: Optional[str] = None,
+        **_
+    ):
+
+        super().__init__(*table, split=split, sample_weight=sample_weight, out=out, jobs=jobs)
+
+        self._classify = classify
+        self._regress = regress
+        self._group = group
+        self._time = time
+        self._ignore = ignore
+        self._config_src = config
+        self._default_config = default_config
+        self._target = None
+
+    def update(self, src: dict = None):
+        super().update(src)
+        if src:
+            if self._classify is None:
+                self._classify = src.get('classify')
+            if self._regress is None:
+                self._regress = src.get('regress')
+            if self._group is None:
+                self._group = src.get('group')
+            if self._time is None:
+                self._time = src.get('time')
+            if self._ignore is None:
+                self._ignore = src.get('ignore')
+            if self._config_src is None:
+                self._config_src = src.get('config')
+            if self._default_config is None:
+                self._default_config = src.get('default_config')
+
+    def resolve(self):
+        if self._group == '':
+            self._group = None
+        if self._config_src == '':
+            self._config_src = None
+        if self._default_config in (None, ''):
+            self._default_config = 'full'
+        self._ignore = set() if self._ignore is None else set(self._ignore)
+
+        if self._classify is None:
+            if self._regress is None:
+                self._target = []
+            else:
+                self._target = [self._regress] if isinstance(self._regress, str) else list(self._regress)
+            self._classify = False
+        elif self._regress is None:
+            self._target = [self._classify] if isinstance(self._classify, str) else list(self._classify)
+            self._classify = True
+        else:
+            raise ValueError('At least one of `classify` and `regress` must be None.')
+
+        if self._out is None:
+            self._out = self._table[0]
+            if isinstance(self._out, pd.DataFrame):
+                raise ValueError('Output directory must be specified when passing a DataFrame.')
+            self._out = self._out.parent / (self._out.stem + '_catabra_' + self._start.strftime('%Y-%m-%d_%H-%M-%S'))
+        else:
+            self.out = io.make_path(self._out, absolute=True)
+
+    def to_dict(self) -> dict:
+        dic = super().to_dict()
+        dic.update(dict(
+            table=['<DataFrame>' if isinstance(tbl, pd.DataFrame) else tbl for tbl in self._table],
+            target=['<DataFrame>' if isinstance(tgt, pd.DataFrame) else tgt for tgt in self._target],
+            classify=self._classify,
+            group=self._group,
+            ignore=self._ignore,
+            config=self._config_src,
+            default_config=self._default_config,
+            time=self._time,
+        ))
+        return dic
--- catabra/evaluation/main.py
+++ catabra/evaluation/main.py
@@ -3,9 +3,8 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 
-from .config import EvaluationInvocation
 from ..util import table as tu, io, logging
-from ..core import CaTabRaBase
+from ..core import CaTabRaBase, Invocation
 from ..util import plotting
 from ..util import metrics
 from ..util import statistics
@@ -69,7 +68,7 @@ def evaluate(*table: Union[str, Path, pd.DataFrame], folder: Union[str, Path] = 
 class Evaluator(CaTabRaBase):
 
     @property
-    def invocation_class(self) -> Type[EvaluationInvocation]:
+    def invocation_class(self) -> Type['EvaluationInvocation']:
         return EvaluationInvocation
 
     def _call(self):
@@ -272,6 +271,160 @@ class Evaluator(CaTabRaBase):
                     yield _m, self._invocation.out / _k
 
         return _iter_splits
+
+
+class EvaluationInvocation(Invocation):
+
+    @property
+    def folder(self) -> Union[str, Path]:
+        return self._folder
+
+    @property
+    def model_id(self) -> Optional[str]:
+        return self._model_id
+
+    @property
+    def explain(self):
+        return self._explain
+
+    @explain.setter
+    def explain(self, value):
+        self._explain = value
+
+    @property
+    def glob(self) -> Optional[bool]:
+        return self._glob
+
+    @property
+    def batch_size(self) -> Optional[int]:
+        return self._batch_size
+
+    @property
+    def threshold(self) -> Optional[float]:
+        return self._threshold
+
+    @property
+    def bootstrapping_repetitions(self) -> Optional[int]:
+        return self._bootstrapping_repetitions
+
+    @bootstrapping_repetitions.setter
+    def bootstrapping_repetitions(self, value: int):
+        self._bootstrapping_repetitions = value
+
+    @property
+    def bootstrapping_metrics(self) -> Optional[list]:
+        return self._bootstrapping_metrics
+
+    def __init__(
+        self,
+        *table,
+        split: Optional[str] = None,
+        sample_weight: Optional[str] = None,
+        out: Union[str, Path, None] = None,
+        jobs: Optional[int] = None,
+        folder: Union[str, Path] = None,
+        model_id=None,
+        explain=None,
+        glob: Optional[bool] = False,
+        batch_size: Optional[int] = None,
+        threshold: Optional[float] = None,
+        bootstrapping_repetitions: Optional[int] = None,
+        bootstrapping_metrics: Optional[list] = None,
+    ):
+
+        super().__init__(*table, split=split, sample_weight=sample_weight, out=out, jobs=jobs)
+        self._folder = folder
+        self._model_id = model_id
+        self._explain = explain
+        self._glob = glob
+        self._batch_size = batch_size
+        self._threshold = threshold
+        self._bootstrapping_repetitions = bootstrapping_repetitions
+        self._bootstrapping_metrics = bootstrapping_metrics
+
+    def update(self, src: dict = None):
+        super().update(src)
+        if src:
+            if self._folder is None:
+                self._folder = src.get('folder')
+            if self._model_id is None:
+                self._model_id = src.get('model_id')
+            if self._explain is None:
+                self._explain = src.get('explain')
+            if self._glob is None:
+                self._glob = src.get('glob')
+            if self._threshold is None:
+                self._threshold = src.get('threshold')
+            if self._bootstrapping_repetitions is None:
+                self._bootstrapping_repetitions = src.get('bootstrapping_repetitions')
+            if self._bootstrapping_metrics is None:
+                self._bootstrapping_metrics = src.get('bootstrapping_metrics')
+            if self._batch_size is None:
+                self._batch_size = src.get('bootstrapping_metrics')
+
+    def resolve(self):
+        if self._threshold is None:
+            self._threshold = 0.5
+
+        if self._out is None:
+            self._out = self._table[0]
+            if isinstance(self._out, pd.DataFrame):
+                self._out = self._folder / ('eval_' + self._start.strftime('%Y-%m-%d_%H-%M-%S'))
+            else:
+                self._out = self._folder / ('eval_' + self._out.stem + '_' + self._start.strftime('%Y-%m-%d_%H-%M-%S'))
+        else:
+            self._out = io.make_path(self._out, absolute=True)
+        if self._out == self._folder:
+            raise ValueError(
+                f'Output directory must differ from CaTabRa directory, but both are "{self._out.as_posix()}".'
+            )
+
+    def to_dict(self) -> dict:
+        dic = super().to_dict()
+        dic.update(dict(
+            table=['<DataFrame>' if isinstance(tbl, pd.DataFrame) else tbl for tbl in self._table],
+            folder=self._folder,
+            model_id=self._model_id,
+            explain=self._explain,
+            glob=self._glob,
+            split=self._split,
+            sample_weight=self._sample_weight,
+            out=self._out,
+            jobs=self._jobs,
+            threshold=self._threshold,
+            bootstrapping_repetitions=self._bootstrapping_repetitions,
+            bootstrapping_metrics=self._bootstrapping_metrics,
+            timestamp=self._start
+        ))
+        return dic
+
+    def set_models_to_explain(self, model):
+        if self._model_id == '__ensemble__':
+            self._model_id = None
+
+        if self._explain is None:
+            self._explain = set()
+        elif isinstance(self._explain, list):
+
+            self._explain = set(self._explain)
+        elif not isinstance(self._explain, set):
+            self._explain = {self._explain}
+        if '__ensemble__' in self._explain:
+            if self._model_id is None:
+                self._explain = None
+            else:
+                self._explain.remove('__ensemble__')
+                self._explain.update(model.model_ids_)
+        elif '__all__' in self._explain:
+            if self._model_id is None:
+                self._explain = None
+            else:
+                self._explain.remove('__all__')
+                self._explain.add(self._model_id)
+        if isinstance(self._explain, set):
+            if self._model_id is not None and any(e != self._model_id for e in self._explain):
+                raise ValueError('Cannot explain models that are not being evaluated.')
+            self._explain = list(self._explain)
 
 
 def evaluate_split(y_true: pd.DataFrame, y_hat: np.ndarray, encoder, directory=None, main_metrics: list = None,
