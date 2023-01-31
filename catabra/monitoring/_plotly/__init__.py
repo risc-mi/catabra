f8ea8bdf85a3c66ed0dd8c669f584685ac6b76ef
Alexander Maletzky <alexander.maletzky@risc-software.at>
2023-01-31 13:47:04 +0100

    + Added training monitor backends

---
+++ catabra/monitoring/_plotly/__init__.py
@@ -0,0 +1,17 @@
+from ..base import TrainingMonitorBackend
+from ...util import logging, plotting
+
+
+def plotly_factory(**kwargs):
+    try:
+        from .backend import PlotlyBackend
+        return PlotlyBackend(**kwargs)
+    except Exception as e:  # noqa
+        if isinstance(e, ImportError) and 'plotly' in e.msg:
+            logging.warn(plotting.PLOTLY_WARNING)
+        else:
+            logging.warn('Loading the plotly training monitor backend failed with the following exception: ' + str(e))
+        return None
+
+
+TrainingMonitorBackend.register('plotly', plotly_factory)
