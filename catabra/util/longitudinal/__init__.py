09f3b1cb6500187edc5e9e6e19b0887859be69b7
Alexander Maletzky <alexander.maletzky@risc-software.at>
2024-09-16 16:11:28 +0200

    replace longitudinal functionality by catabra-pandas

--- catabra/util/longitudinal/__init__.py
+++
@@ -1,7 +0,0 @@
-#  Copyright (c) 2022. RISC Software GmbH.
-#  All rights reserved.
-
-from ._misc import group_temporal, prev_next_values
-from ._resampling import resample_eav, resample_interval, make_windows
-
-__all__ = ['resample_eav', 'resample_interval', 'make_windows', 'group_temporal', 'prev_next_values']
